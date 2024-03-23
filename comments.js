c769ed914fbd9c882eb3a971d859851d7030be39
Rohan <rohandiwan1824@gmail.com>
2024-03-23 07:38:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,62 @@
+// Create web server and listen for requests
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
+// Create a server
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+    // Get the file path
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    
+    // Get the file extension
+    let extname = path.extname(filePath);
+
+    // Set the content type
+    let contentType = 'text/html';
+
+    // Check the extension and set the content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    // Read the file
+    fs.readFile(filePath, (err, content) => {
+        // Check for error
+        if (err) {
+            if (err.code == 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                });
+            } else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+// Listen on port 3000
+server.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
 });
